103222f5739996713789cea4f9731bf6852a2539
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-01-11 21:25:56 -0200

    feat(infra): adição da biblioteca de objetos funcionais
    Incluído pacote com as classes da biblioteca de programação funcional do Vladimir Khorikov

---
+++ src/AssistenteFinanceiro.Infra.Functional/Async/AsyncMaybeExtensions.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+
+namespace InsurSoft.Backend.Shared.Functional.Async
+{
+    public static class AsyncMaybeExtensions
+    {
+        public static async Task<Result<T>> ToResult<T>(this Task<Maybe<T>> maybeTask, string[] errorMessages, bool continueOnCapturedContext = true)
+            where T : class
+        {
+            Maybe<T> maybe = await maybeTask.ConfigureAwait(continueOnCapturedContext);
+            return maybe.ToResult(errorMessages);
+        }
+    }
+}
---
+++ src/AssistenteFinanceiro.Infra.Functional/IResult.cs
@@ -0,0 +1,8 @@
+﻿namespace InsurSoft.Backend.Shared.Functional
+{
+    public interface IResult
+    {
+        bool IsFailure { get; }
+        bool IsSuccess  { get; }
+    }
+}
