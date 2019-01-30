8740636ea99a8e129c895fab8933348faa5855f9
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-01-30 21:10:01 -0200

    feat(contas): implementação da adição de contas no repositório

--- src/AssistenteFinanceiro.Application.Infra.Repositories/ContasRepository.cs
+++ src/AssistenteFinanceiro.Application.Infra.Repositories/ContasRepository.cs
@@ -1,13 +1,22 @@
 ﻿using AssistenteFinanceiro.Application.Interfaces.Repositories;
 using AssistenteFinanceiro.Domain.Model;
+using AssistenteFinanceiro.Infra.Database.Context;
 
 namespace AssistenteFinanceiro.Application.Infra.Repositories
 {
     public class ContasRepository : IContasRepository
     {
+        private readonly RhittaContext _context;
+
+        public ContasRepository(RhittaContext context)
+        {
+            _context = context;
+        }
+
         public void AdicionarConta(Conta conta)
         {
-            throw new System.NotImplementedException();
+            _context.Contas.Add(conta);
+            _context.SaveChanges();
         }
     }
 }
