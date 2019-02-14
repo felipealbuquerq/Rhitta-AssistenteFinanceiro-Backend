dac8aa557a9facdaa20ab71d0d441ea3226f4f50
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-02-14 13:23:18 -0200

    feat(extrato-mensal): modelagem inicial do extrato

--- src/AssistenteFinanceiro.Application/ExtratosMensais/QueriesResponses/ExtratoMensal.cs
+++ src/AssistenteFinanceiro.Application/ExtratosMensais/QueriesResponses/ExtratoMensal.cs
@@ -1,6 +1,20 @@
-﻿namespace AssistenteFinanceiro.Application.ExtratosMensais.QueriesResponses
+﻿using System.Collections.Generic;
+
+namespace AssistenteFinanceiro.Application.ExtratosMensais.QueriesResponses
 {
     public class ExtratoMensal
     {
+        public decimal SaldoAtual { get; private set; }
+        public decimal SaldoPrevisto { get; private set; }
+        public List<Transacao> Transacoes { get; private set; }
+
+        public int TransacoesPendentes => 0;
+        public int TransacoesFinalizadas => 0;
+
+        public class Transacao
+        {
+            public string Descricao { get; set; }
+            public decimal Valor { get; set; }
+        }
     }
 }
