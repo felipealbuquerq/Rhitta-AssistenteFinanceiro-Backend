8790da122a56b84ff36a4073654c2d87efca3658
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-02-14 20:38:20 -0200

    feat(extrato-mensal): implementação inicial do repositório

---
+++ src/AssistenteFinanceiro.Application.Infra.Repositories/ExtratoMensalRepository.cs
@@ -0,0 +1,44 @@
+﻿using AssistenteFinanceiro.Application.ExtratosMensais.Interfaces.Repositories;
+using AssistenteFinanceiro.Application.ExtratosMensais.QueriesResponses;
+using AssistenteFinanceiro.Infra.Database.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace AssistenteFinanceiro.Application.Infra.Repositories
+{
+    public class ExtratoMensalRepository : IExtratoMensalRepository
+    {
+        private readonly RhittaContext _context;
+
+        public ExtratoMensalRepository(RhittaContext context)
+        {
+            _context = context;
+        }
+
+        public ExtratoMensal ObterExtratoMensal(int ano, int mes)
+        {
+            var transacoesDoMes = _context
+                .Transacoes
+                .AsNoTracking()
+                .Where(c => c.DataLancamento.Data.Month == mes && c.DataLancamento.Data.Year == ano)
+                .Select(c => new ExtratoMensal.Transacao(
+                    "Descrição pendente",
+                    c.Valor,
+                    c.DataLancamento.Data,
+                    c.Efetivada
+                ))
+                .ToList();
+
+            var saldoAtual = _context
+                .Contas
+                .AsNoTracking()
+                .Sum(c => c.SaldoAtual);
+
+            var saldoNaoEfetivado = transacoesDoMes
+                .Where(t => !t.Efetivada)
+                .Sum(t => t.Valor);
+
+            return new ExtratoMensal(saldoAtual, saldoAtual + saldoNaoEfetivado, transacoesDoMes);
+        }
+    }
+}
--- src/AssistenteFinanceiro.Application/ExtratosMensais/QueriesResponses/ExtratoMensal.cs
+++ src/AssistenteFinanceiro.Application/ExtratosMensais/QueriesResponses/ExtratoMensal.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AssistenteFinanceiro.Application.ExtratosMensais.QueriesResponses
 {
     public class ExtratoMensal
     {
+        public ExtratoMensal(decimal saldoAtual, decimal saldoPrevisto, List<Transacao> transacoes)
+        {
+            SaldoAtual = saldoAtual;
+            SaldoPrevisto = saldoPrevisto;
+            Transacoes = transacoes;
+        }
+
         public decimal SaldoAtual { get; private set; }
         public decimal SaldoPrevisto { get; private set; }
         public List<Transacao> Transacoes { get; private set; }
 
-        public int TransacoesPendentes => 0;
-        public int TransacoesFinalizadas => 0;
+        public int TransacoesPendentes => Transacoes.Count(t => !t.Efetivada);
+        public int TransacoesFinalizadas => Transacoes.Count(t => t.Efetivada);
 
         public class Transacao
         {
+            public Transacao(string descricao, decimal valor, DateTime dataEfetivacao, bool efetivada)
+            {
+                Descricao = descricao;
+                Valor = valor;
+                DataEfetivacao = dataEfetivacao;
+                Efetivada = efetivada;
+            }
+
             public string Descricao { get; set; }
             public decimal Valor { get; set; }
+            public DateTime DataEfetivacao { get; set; }
+            public bool Efetivada { get; set; }
         }
     }
 }
