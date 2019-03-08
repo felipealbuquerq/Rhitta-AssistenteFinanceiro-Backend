14d4fc5e845654d4c93e112d3dcddaa3927ada14
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-03-08 14:07:04 -0300

    feat(transacoes): criação do controller de transações

---
+++ src/AssistenteFinanceiro.Api/Controllers/TransacoesController.cs
@@ -0,0 +1,33 @@
+﻿using AssistenteFinanceiro.Api.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AssistenteFinanceiro.Api.Controllers
+{
+    [ApiController]
+    [Route("api/Contas/{codigoConta:guid}/Transacoes")]
+    public class TransacoesController : ApiController
+    {
+        [HttpGet]
+        public ActionResult Get(Guid codigoConta)
+        {
+            return Ok(codigoConta);
+        }
+
+        [HttpGet("{codigoTransacao:guid}")]
+        public ActionResult Get(Guid codigoConta, Guid codigoTransacao)
+        {
+            return Ok(new
+            {
+                codigoConta,
+                codigoTransacao
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Post(Guid codigoConta)
+        {
+            return Ok(codigoConta);
+        }
+    }
+}
